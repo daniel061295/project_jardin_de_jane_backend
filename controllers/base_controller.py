2f7749e6d584d0f83fe7d1ffdf50390621f34ce0
Daniel <daniel061295@gmail.com>
2025-09-06 06:42:24 -0500

    Se corrige error en rutas

--- controllers/base_controller.py
+++ controllers/base_controller.py
@@ -1,4 +1,5 @@
 from abc import ABC
+from typing import Callable, List
 from fastapi import APIRouter
 
 class BaseController(ABC):
@@ -8,3 +9,19 @@ class BaseController(ABC):
     
     def get_router(self) -> APIRouter:
         return self.router
+
+    def add_api_route(self, path: str, endpoint: Callable, methods: List[str]):
+        """
+        Registra la ruta con y sin slash final automáticamente.
+        Ejemplo:
+            path="/" -> /products y /products/
+            path="/{id}" -> /products/{id} (no duplica)
+        """
+        # Si es ruta raíz ("" o "/") → registrar las dos variantes
+        if path in ("", "/"):
+            clean_path = ""
+            self.router.add_api_route(clean_path, endpoint, methods=methods)
+            self.router.add_api_route("/", endpoint, methods=methods)
+        else:
+            # Para rutas con parámetros u otras, solo una vez
+            self.router.add_api_route(path, endpoint, methods=methods)
